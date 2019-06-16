7842a15e063cc1b1d57ee694ea91a38a6dfa72b0
Eduard-z <chegladze@mail.ru>
2019-06-16 19:55:44 +0300

    removed "both_scored_first_half", "total_second_half"

--- store.py
+++ store.py
@@ -1,93 +1,83 @@
 import re
 from draft import Team
 
-file = open(r'C:\Users\Admin\Desktop\soccer\belarus.txt', encoding='utf-8')
-list_of_teams = set()
-for line in file:
-    game = re.split("\t|\s\-\s|\W\(|\)|\n", line)
-    game[1] = game[1].strip()
-    game[2] = game[2].strip()
-    game.remove('')
-    # print(game)
-    # break
-    Team.meetings.append(game)
-    list_of_teams.add(game[1])  # list of team names
-Team.meetings.reverse()
+list_of_champs = ['belarus', 'brazilA', 'brazilB', 'china', 'ecuador', 'estonia', 'finland', 'finland2', 'iceland1',
+                  'icelandP', 'indonesia', 'ireland1', 'irelandP', 'japan', 'japan2', 'kazahstan', 'koreaK', 'koreaKC',
+                  'latvia', 'lithuania', 'malaysia', 'MLS', 'moldova', 'norway2', 'norwayP', 'singapur', 'sweden2',
+                  'swedenP', 'thailand', 'vietnam']
 
-print(list_of_teams)  # test
-list_of_objects = []  # list of object names
-for i in list_of_teams:
-    temp = Team(i)
-    list_of_objects.append(temp)
-Team.teams = list_of_objects
+for championship in list_of_champs:
+    path_to_results = r'C:\Users\Admin\Desktop\soccer\{champ}.txt'.format(champ=championship)
+    file = open(path_to_results, encoding='utf-8')
+    list_of_teams = set()
+    for line in file:
+        game = re.split("\t|\s\-\s|\W\(|\)|\n", line)
+        game[1] = game[1].strip()
+        game[2] = game[2].strip()
+        game.remove('')
+        # print(game)
+        # break
+        Team.meetings.append(game)
+        list_of_teams.add(game[1])  # list of team names
+    Team.meetings.reverse()
 
-# Wolfsburg = Team('Вольфсбург')
-# Cologne = Team('Кельн')
-# Leverkusen = Team('Байер Л')
-# Hannover = Team('Ганновер 96')
-# Hoffenheim = Team('Хоффенхайм')
-# Dortmund = Team('Боруссия Д')
-# Bayern_Munich = Team('Бавария')
-# Stuttgart = Team('Штуттгарт')
-# Schalke_04 = Team('Шальке 04')
-# Eintracht_Frankfurt = Team('Айнтрахт Фр')
-# Freiburg = Team('Фрайбург')
-# Augsburg = Team('Аугсбург')
-# Hertha = Team('Герта')
-# Leipzig = Team('Лейпциг')
-# Hamburg = Team('Гамбург')
-# M_gladbach = Team('Боруссия М')
-# Mainz_05 = Team('Майнц 05')
-# Werder_Bremen = Team('Вердер')
-# Team.teams.extend([Werder_Bremen, Wolfsburg, Cologne, Leverkusen, Hannover, Hoffenheim, Dortmund, Bayern_Munich, Mainz_05,
-#                    Stuttgart, Schalke_04, Eintracht_Frankfurt, Freiburg, Augsburg, Hertha, Leipzig, Hamburg, M_gladbach])
+    print(list_of_teams)  # test
+    list_of_objects = []  # list of object names
+    for i in list_of_teams:
+        temp = Team(i)
+        list_of_objects.append(temp)
+    Team.teams = list_of_objects
 
-for each in Team.teams:
-    for game in Team.meetings:
-        if each.name == game[1] or each.name == game[2]:
-            each.meetings.append(game)
-print(Team.meetings)  # test
+    for each in Team.teams:
+        for game in Team.meetings:
+            if each.name == game[1] or each.name == game[2]:
+                each.meetings.append(game)
+    print(Team.meetings)  # test
 
-# print(Dortmund.game_score())  # test
-for each in Team.teams:
-    print(each.game_score())
-    for indicator in ["self.scored", "self.scored1", "self.scored2", "self.conceded", "self.conceded1", "self.conceded2",
-                      "total_first_half", "total_second_half", "both_scored", "both_scored_first_half",
-                      "both_scored_second_half"]:
-        for param in [0.5]:
-            for above in [">", "<"]:
-                for place in ["all", "home", "away"]:
-                    if len(each.series_above(indicator, param, above, place)) > 2:
-                        print(each.series_above(indicator, param, above, place), each.name, indicator, above, param,
-                              place)
+    # print(Dortmund.game_score())  # test
+    for each in Team.teams:
+        print(each.game_score())
+        for indicator in ["self.scored", "self.scored1", "self.scored2", "self.conceded", "self.conceded1", "self.conceded2",
+                          "total_first_half", "both_scored", "both_scored_second_half"]:
+            # "both_scored_first_half", "total_second_half"
+            for param in [0.5]:
+                for above in [">", "<"]:
+                    for place in ["all", "home", "away"]:
+                        if len(each.series_above(indicator, param, above, place)) > 2:
+                            print(each.series_above(indicator, param, above, place), each.name, indicator, above, param,
+                                  place, file=open(r'C:\Users\Admin\Desktop\test_belarus.txt', 'a', encoding='utf-8'))
 
-    for indicator in ["self.scored", "self.conceded", "total_first_half", "total_second_half"]:
-        for param in [1.5]:
-            for above in [">", "<"]:
-                for place in ["all", "home", "away"]:
-                    if len(each.series_above(indicator, param, above, place)) > 2:
-                        print(each.series_above(indicator, param, above, place), each.name, indicator, above, param, place)
+        for indicator in ["self.scored", "self.conceded", "total_first_half", "total_second_half"]:
+            for param in [1.5]:
+                for above in [">", "<"]:
+                    for place in ["all", "home", "away"]:
+                        if len(each.series_above(indicator, param, above, place)) > 2:
+                            print(each.series_above(indicator, param, above, place), each.name, indicator, above, param,
+                                  place, file=open(r'C:\Users\Admin\Desktop\test_belarus.txt', 'a', encoding='utf-8'))
 
-    for indicator in ["total"]:
-        for param in [1.5, 2.5, 3.5]:
-            for above in [">", "<"]:
-                for place in ["all", "home", "away"]:
-                    if len(each.series_above(indicator, param, above, place)) > 2:
-                        print(each.series_above(indicator, param, above, place), each.name, indicator, above, param, place)
+        for indicator in ["total"]:
+            for param in [1.5, 2.5, 3.5]:
+                for above in [">", "<"]:
+                    for place in ["all", "home", "away"]:
+                        if len(each.series_above(indicator, param, above, place)) > 2:
+                            print(each.series_above(indicator, param, above, place), each.name, indicator, above, param,
+                                  place, file=open(r'C:\Users\Admin\Desktop\test_belarus.txt', 'a', encoding='utf-8'))
 
-    for indicator in ["self.result"]:
-        for param in [0.5, 1.5, -0.5, -1.5]:
-            for above in [">", "<"]:
-                for place in ["all", "home", "away"]:
-                    if len(each.series_above(indicator, param, above, place)) > 2:
-                        print(each.series_above(indicator, param, above, place), each.name, indicator, above, param, place)
+        for indicator in ["self.result"]:
+            for param in [0.5, 1.5, -0.5, -1.5]:
+                for above in [">", "<"]:
+                    for place in ["all", "home", "away"]:
+                        if len(each.series_above(indicator, param, above, place)) > 2:
+                            print(each.series_above(indicator, param, above, place), each.name, indicator, above, param,
+                                  place, file=open(r'C:\Users\Admin\Desktop\test_belarus.txt', 'a', encoding='utf-8'))
 
-    for indicator in ["self.result_first_half", "self.result_second_half"]:
-        for param in [0.5, -0.5]:
-            for above in [">", "<"]:
-                for place in ["all", "home", "away"]:
-                    if len(each.series_above(indicator, param, above, place)) > 2:
-                        print(each.series_above(indicator, param, above, place), each.name, indicator, above, param, place)
+        for indicator in ["self.result_first_half", "self.result_second_half"]:
+            for param in [0.5, -0.5]:
+                for above in [">", "<"]:
+                    for place in ["all", "home", "away"]:
+                        if len(each.series_above(indicator, param, above, place)) > 2:
+                            print(each.series_above(indicator, param, above, place), each.name, indicator, above, param,
+                                  place, file=open(r'C:\Users\Admin\Desktop\test_belarus.txt', 'a', encoding='utf-8'))
 
 
 """
